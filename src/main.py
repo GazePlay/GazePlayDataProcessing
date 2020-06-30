e5498584e9cf720e87bef304cf08fd783c55d27c
christophe <christophezeinaty@hotmail.com>
2020-06-30 19:34:09 +0200

    -added method to restructure the data file. -added method to compute velocity in degree/sec. -added method to classify fixations.

--- src/main.py
+++ src/main.py
@@ -1,9 +1,24 @@
-import src.util as helper
+import src.dataprocessing as helper
+
+
+def processData():
+    data = helper.readJsonDataFromFile('data/data.json')
+    velocityThreshold = helper.readConfig('config/config.json')
+    coord = helper.restructureCoordinatesFormat(data)
+    distance = helper.computeDistance(coord)
+    velocities = helper.computeVelocity(distance)
+    fixation = helper.classifyVelocities(velocities, velocityThreshold)
+    fixationGroups = list(helper.groupConsecutiveFixation(fixation))
+    print("velocityThreshold: ", velocityThreshold)
+    print("coordinates: ", coord)
+    print("distance: ", distance)
+    print("velocities: ", velocities)
+    print("fixation: ", fixation)
+    print("fixation groups: ", str(fixationGroups))
 
 
 def main():
-    data = helper.readJsonDataFromFile("/home/christophe/PycharmProjects/gazePlayDataProcessing/data/data.json")
-    print(data)
+    processData()
 
 
 if __name__ == '__main__':
